7a50d4bf18b80289a17143c39384c9e13169873a
root-marco <rootslowed@gmail.com>
2022-03-01 14:02:31 -0300

    constructor to handle getter and setter of the grid

--- Tetris/GameGrid.cs
+++ Tetris/GameGrid.cs
@@ -5,5 +5,11 @@
         private readonly int[,] grid;
         public int Rows { get; }
         public int Columns { get; }
+
+        public int this[int rows, int columns]
+        {
+            get => grid[rows, columns];
+            set => grid[rows, columns] = value;
+        }
     }
 }
